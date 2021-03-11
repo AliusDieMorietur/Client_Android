e3076a8b8f6ff7801d1db0bae451ccc7b745ae33
KirillEmets <kirill.yemets@gmail.com>
2021-03-11 14:21:57 +0200

    Настройка для выбора пути сохранения.

--- app/src/main/java/com/samurainomichi/cloud_storage_client/settings/SettingsFragment.kt
+++ app/src/main/java/com/samurainomichi/cloud_storage_client/settings/SettingsFragment.kt
@@ -1,11 +1,45 @@
 package com.samurainomichi.cloud_storage_client.settings
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.samurainomichi.cloud_storage_client.R
+import com.samurainomichi.cloud_storage_client.openDirectory
+import java.io.File
+import java.net.URI
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        val prefIp = findPreference<EditTextPreference>("server_ip")
+        prefIp?.summaryProvider = EditTextPreference.SimpleSummaryProvider.getInstance()
+
+        val prefPath = findPreference<Preference>("download_path")
+        prefPath?.summary = preferences.getString("download_path", null)?.let { Uri.parse(it).path } ?: "Not set"
+
+
+        val openDocumentTree = registerForActivityResult(ActivityResultContracts.OpenDocumentTree()) { uri ->
+            requireContext().contentResolver.takePersistableUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            val path = uri.toString()
+            with(preferences.edit()) {
+                putString("download_path", path)
+                apply()
+            }
+            prefPath?.summary = preferences.getString("download_path", null)?.let { Uri.parse(it).path } ?: "Not set"
+        }
+        prefPath?.setOnPreferenceClickListener {
+            openDocumentTree.launch(null)
+            true
+        }
+
     }
 }
