5be3349162bad727a3357e5a60aca0d315489735
KirillEmets <kirill.yemets@gmail.com>
2021-03-11 14:21:05 +0200

    openDirectory function added

--- app/src/main/java/com/samurainomichi/cloud_storage_client/MyUtil.kt
+++ app/src/main/java/com/samurainomichi/cloud_storage_client/MyUtil.kt
@@ -2,11 +2,17 @@ package com.samurainomichi.cloud_storage_client
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 
 fun checkPermission(context: Context, permission: String): Boolean =
         (ContextCompat.checkSelfPermission(
             context,
             permission) == PackageManager.PERMISSION_GRANTED)
 
+fun openDirectory(context: Context) {
+    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+    context.startActivity(intent)
+}
