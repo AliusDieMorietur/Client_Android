99a6e04a5902ae80c5d76c721b94e5853102de8c
KirillEmets <kirill.yemets@gmail.com>
2021-04-28 12:27:10 +0300

    implemented tests for permanent storage.

--- app/src/test/java/com/samurainomichi/cloud_storage_client/PmtWebSocketTest.kt
+++ app/src/test/java/com/samurainomichi/cloud_storage_client/PmtWebSocketTest.kt
@@ -1,53 +1,104 @@
 package com.samurainomichi.cloud_storage_client
-//
-//import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-//import com.samurainomichi.cloud_storage_client.network.Connection
-//import com.samurainomichi.cloud_storage_client.network.WebSocketDataSource
-//import kotlinx.coroutines.*
-//import org.junit.*
-//import org.junit.Assert.*
-//import org.junit.runners.MethodSorters
-//
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-//class PmtWebSocketTest {
-//    companion object {
-//        private const val ip = "127.0.0.1:8000"
-//        private val connection = Connection.getInstance(WebSocketDataSource(ip))
-//        private var token: String = ""
-//    }
-//
-//    @Rule
-//    @JvmField
-//    val instantTaskExecutorRule = InstantTaskExecutorRule()
-//
-//    @Test
-//    fun t0_connect() {
-//        connection.connectBlocking()
-//        println("Connected")
-//    }
-//
-//    @Test
-//    fun t1_auth() = runBlocking {
-//        val authToken = connection.authLogin("admin", "admin")
-//        println("Logged in")
-//        println("Auth token: $authToken")
-//        assertEquals(32, authToken.length)
-//    }
-//
-//    @Test
-//    fun t2_newFolder() = runBlocking {
-//
-//    }
-//
-//    @Test
-//    fun t2_availableFiles() = runBlocking {
-//        val structure = connection.pmtAvailableFiles()
-////        println(structure.size)
-//    }
-//
-//    @Test
-//    fun tl0_logOut() = runBlocking {
-//        connection.authLogout()
-//        println("Logged out")
-//    }
-//}
+
+import com.samurainomichi.cloud_storage_client.network.ConnectionRepository
+import com.samurainomichi.cloud_storage_client.network.WebSocketDataSource
+import kotlinx.coroutines.*
+import org.junit.*
+import org.junit.Assert.*
+import org.junit.runners.MethodSorters
+import java.lang.Exception
+import java.nio.ByteBuffer
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class PmtWebSocketTest {
+    companion object {
+        private const val ip = "192.168.1.148:7000"
+        private val repository = ConnectionRepository.getInstance(WebSocketDataSource(ip))
+        private var token: String = ""
+        private var authToken: String = ""
+    }
+
+    @Test
+    fun t0_connect() {
+        repository.connectBlocking()
+        println("Connected")
+    }
+
+    @Test
+    fun t10_auth() = runBlocking {
+        repository.authCreateUser("testUser", "testPassword")
+        authToken = repository.authLogin("testUser", "testPassword")
+        println("Logged in")
+        println("Auth token: $authToken")
+        assertEquals(32, authToken.length)
+    }
+
+    @Test
+    fun t20_newFolder() = runBlocking {
+        repository.pmtNewFolder("foo/")
+        repository.pmtNewFolder("bar/")
+        repository.pmtNewFolder("foo/second/")
+
+        val structure = repository.pmtAvailableStructure()
+        assertEquals(2, structure.size)
+        assertEquals("second", structure.find { it.name == "foo" }?.children?.get(0)?.name)
+    }
+
+    @Test
+    fun t21_newFolderSameName() = runBlocking {
+        repository.pmtNewFolder("foo/")
+
+        val structure = repository.pmtAvailableStructure()
+        assertEquals(2, structure.size)
+    }
+
+    @Test
+    fun t30_upload() = runBlocking {
+        repository.pmtUploadFilesStart(listOf("bar/File1", "bar/File2"))
+        val buffer = ByteBuffer.wrap(ByteArray(25) { i -> (i % 16).toByte() })
+        repository.sendBuffer(buffer.asReadOnlyBuffer())
+        repository.sendBuffer(buffer.asReadOnlyBuffer())
+        repository.pmtUploadFilesEnd(listOf("bar/File1", "bar/File2"))
+
+        val structure = repository.pmtAvailableStructure()
+        val bar = structure.find { it.name == "bar" }!!
+
+        assertEquals(2, bar.children?.size)
+        assertEquals(50, bar.capacity)
+    }
+
+    @Test
+    fun t40_rename() = runBlocking {
+        repository.pmtRenameFile("bar/File1", "bar/newName")
+        val structure = repository.pmtAvailableStructure()
+        val foo = structure.find { it.name == "bar" }!!
+
+        assertEquals(null, foo.children?.find { it.name == "File1" }?.name)
+        assertEquals("newName", foo.children?.find { it.name == "newName" }?.name)
+        assertEquals("File2", foo.children?.find { it.name == "File2" }?.name)
+    }
+
+    @Test
+    fun t41_renameSameName() = runBlocking {
+        try {
+            repository.pmtRenameFile("bar/File2", "bar/newName")
+            fail()
+        }
+        catch (e: Exception) {
+            assertEquals("NOT_IMPLEMENTED", e.message)
+        }
+
+    }
+
+    @Test
+    fun t50_availableFiles() = runBlocking {
+        val structure = repository.pmtAvailableStructure()
+        assertEquals(50, structure.sumBy { it.capacity })
+    }
+
+    @Test
+    fun tl0_deleteUser() = runBlocking {
+        repository.authDeleteUser()
+        println("Logged out")
+    }
+}
